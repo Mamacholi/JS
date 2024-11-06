0be384f4e96c63c8bf4e8519a50ec1357643b6c8
Mamacholi <drive83a@gmail.com>
2024-11-06 17:09:39 +0200

    change home work

--- JS Lessons/JS Lesson 1/JS HW 1-2/main.js
+++ JS Lessons/JS Lesson 1/JS HW 1-2/main.js
@@ -1,5 +1,5 @@
 let firstName = 'Roman';
-let middleName =' ';
+let middleName = 'Roma';
 let lastName = 'Kuspis';
-let person = firstName + middleName + lastName;
+let person = firstName + ' ' + middleName + ' ' + lastName;
 console.log(person);
