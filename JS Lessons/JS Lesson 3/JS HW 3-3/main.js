e4f779cc94a432112e79691ea6a490cbb9611fed
Mamacholi <drive83a@gmail.com>
2024-11-07 22:45:47 +0200

    add home work 3

---
+++ JS Lessons/JS Lesson 3/JS HW 3-3/main.js
@@ -0,0 +1,7 @@
+// #uzkt71dp
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+let i = 0;
+while (i < 20) {
+    document.write('<h1>programming is okten</h1>');
+    i++;
+}
